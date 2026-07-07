473eceff5f80546752a44c8e4c579c752e7c8f3b
John Tuley <johntuley@render.com>
2026-07-07 14:20:57 -0600

    initial template

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { task } from "@renderinc/sdk/workflows";
+
+const calculateSquare = task(
+  { name: "calculateSquare" },
+  function calculateSquare(a: number): number {
+    return a * a;
+  },
+);
+
+const sumSquares = task(
+  { name: "sumSquares" },
+  async function sumSquares(a: number, b: number): Promise<number> {
+    const [result1, result2] = await Promise.all([
+      calculateSquare(a),
+      calculateSquare(b),
+    ]);
+    return result1 + result2;
+  },
+);
+
+task(
+  {
+    name: "flipCoin",
+    retry: {
+      maxRetries: 3,
+      waitDurationMs: 1000,
+      backoffScaling: 1.5,
+    },
+  },
+  function flipCoin(): string {
+    if (Math.random() < 0.5) {
+      throw new Error("Flipped tails! Retrying.");
+    }
+    return "Flipped heads!";
+  },
+);
